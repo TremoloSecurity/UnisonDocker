417301c6de3bd856056102d356767e0189c4eb1e
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2016-08-05 10:23:47 -0400

    Update startUnisonInDocker.sh

--- scripts/startUnisonInDocker.sh
+++ scripts/startUnisonInDocker.sh
@@ -23,12 +23,5 @@ export CLASSPATH=$CLASSPATH:$TREMOLO_ROOT/libs/*:$TREMOLO_ROOT/jars/*:$TREMOLO_H
 
 TREMOLO_CMD="$JAVA_CMD -Dorg.eclipse.jetty.server.Request.maxFormContentSize=-1 -Dcom.tremolosecurity.openunison.activemqdir=$TREMOLO_ACTIVEMQ -Dcom.tremolosecurity.openunison.quartzdir=$TREMOLO_QUARTZ_DIR $JAVA_OPTS -server com.tremolosecurity.server.Server"
 
-echo "Starting Xvfb..."
-
-rm -f /tmp/.X0-lock
-
-#Start Xvfb
-/usr/bin/Xvfb :0.0 >& /dev/null &
-
 echo "Starting Unison..."
 exec $TREMOLO_CMD 2>&1
